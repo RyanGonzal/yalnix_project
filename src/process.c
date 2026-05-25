a9c2d39ad7f15f309c36430a571c67625b34c3b1
Ryan Gonzalez <f006bkc@babylon1.thayer.dartmouth.edu>
2026-05-25 15:34:34 -0400

    cp 4 queues fixes in process.c

--- src/process.c
+++ src/process.c
@@ -8,35 +8,42 @@ pcb_t *current_process = NULL;
 static pcb_t idle_pcb;
 static pcb_t init_pcb;
 pcb_t *init_process = NULL;
-
 typedef struct {
-    pcb_t front;
-    pcb_t back;
+    pcb_t *front;
+    pcb_t *back;
 } pcb_queue_t;
+static pcb_queue_t ready_queue;
+static pcb_queue_t blocked_queue;
+
+
+
 
 void init_queue(pcb_queue_t *rq) {
     rq->front = NULL;
     rq->back = NULL;
 }
 
-void enqueue(pcb_queue_t *rq, pcb_t pcb) {
+void enqueue(pcb_queue_t *rq, pcb_t *pcb) {
     pcb->next = NULL;
 
     if (rq->back != NULL) rq->back->next = pcb;
 
     rq->back = pcb;
 
-    if (rq->front != NULL) rq->front = pcb;
+    if (rq->front == NULL) rq->front = pcb;
 }
 
-void dequeue(pcb_queue_t *rq, pcb_t pcb) {
-    if (rq->front == NULL) return 0;
+pcb_t *dequeue(pcb_queue_t *rq) {
+    if (rq->front == NULL) return NULL;
 
-    pcb_t dequeued = rq->front;
-
+    pcb_t *dequeued = rq->front;
     rq->front = rq->front->next;
 
-    if (rq->front == NULL) rq->front = rq->back;
+    if (rq->front == NULL) rq->back = NULL;
+
+    dequeued->next = NULL;
+
+    return dequeued;
 }
 
 
@@ -46,43 +53,49 @@ void process_init(void) {
     // initialize ready queue
     // initialize blocked queues
 
-    pcb_queue_t *ready_queue = malloc(sizeof(pcb_queue_t));
-    if (ready_queue == NULL) return ERROR;
+    // pcb_queue_t *ready_queue = malloc(sizeof(pcb_queue_t));
+    // if (ready_queue == NULL) return ERROR;
     
-    pcb_queue_t *blocked_queue = malloc(sizeof(pcb_queue_t));
-    if (blocked_queue == NULL) return ERROR;
+    // pcb_queue_t *blocked_queue = malloc(sizeof(pcb_queue_t));
+    // if (blocked_queue == NULL) return ERROR;
 
     // set current_process to NULL
     // prepare for idle
     //  PCB creation
-    current_process = NULL;
-    init_process = NULL;
+    // current_process = NULL;
+    // init_process = NULL;
 
     //  PCB creation
     // Nothing below this is correct
-    pcb_t new_pcb = malloc(sizeof(new_pcb_t));
-    if (new_pcb == NULL) return ERROR;  
+    // pcb_t new_pcb = malloc(sizeof(new_pcb_t));
+    // if (new_pcb == NULL) return ERROR;  
 
-    new_pcb.region1_pt = memory_get_region1_pt();
-    new_pcb.pid = helper_new_pid(idle_pcb.region1_pt);
-    new_pcb.state = PROC_READY;
-    new_pcb.region1_pt = memory_get_region1_pt();
-    new_pcb.parent = NULL;
-    new_pcb.children = NULL;
-    new_pcb.next_sibling = NULL;
-    new_pcb.next = NULL;
-    new_pcb.exit_status = 0;
-    new_pcb.waiting_for_child = 0;
+    // new_pcb.region1_pt = memory_get_region1_pt();
+    // new_pcb.pid = helper_new_pid(idle_pcb.region1_pt);
+    // new_pcb.state = PROC_READY;
+    // new_pcb.region1_pt = memory_get_region1_pt();
+    // new_pcb.parent = NULL;
+    // new_pcb.children = NULL;
+    // new_pcb.next_sibling = NULL;
+    // new_pcb.next = NULL;
+    // new_pcb.exit_status = 0;
+    // new_pcb.waiting_for_child = 0;
 
     // save current user context
-    UserContext *uctxt = malloc(sizeof(UserContext))
-    if (uctxt == NULL) return ERROR;  
+    // UserContext *uctxt = malloc(sizeof(UserContext))
+    // if (uctxt == NULL) return ERROR;  
 
-    new_pcb.user_context = *uctxt;
+    // new_pcb.user_context = *uctxt;
 
-    enqueue(ready_queue, new_pcb);
+    // enqueue(ready_queue, new_pcb);
 
     // prepare for idle
+    // we dont need to create a pcb, do we even need this function?
+    current_process = NULL;
+    init_process = NULL;
+
+    init_queue(&ready_queue);
+    init_queue(&blocked_queue);
 }
 
 // process_create_idle creates the idle process
@@ -102,6 +115,7 @@ pcb_t *process_create_idle(UserContext *uctxt)
     idle_pcb.next = NULL;
     idle_pcb.exit_status = 0;
     idle_pcb.waiting_for_child = 0;
+    idle_pcb.delay_ticks = 0;
 
     //save current user context
     idle_pcb.user_context = *uctxt;
@@ -132,7 +146,7 @@ pcb_t *process_create_init(UserContext *uctxt)
     init_pcb.user_context = *uctxt;
 
     init_process = &init_pcb;
-
+    scheduler_add(&init_pcb);
     return &init_pcb;
 }
 
@@ -176,11 +190,16 @@ pcb_t *scheduler_next(void)
     // else return idle process
 
     //for now if init ready run init, else idle 
-    if (init_process != NULL && init_process->state == PROC_READY) {
-        return init_process;
+    // if (init_process != NULL && init_process->state == PROC_READY) {
+    //     return init_process;
+    // }
+    // return &idle_pcb;
+    //above was cp3
+    pcb_t *next = dequeue(&ready_queue);
+    if (next != NULL) {
+        return next;
     }
-
-    return &idle_pcb;
+    return process_get_idle();
 
 }
 
@@ -190,10 +209,11 @@ void scheduler_add(pcb_t *proc)
     // mark process as READY
     // add process to ready queue
     //for now just set to ready
-    if (proc == NULL) {
+    if (proc == NULL || proc == process_get_idle()) {
         return;
     }
     proc->state = PROC_READY;
+    enqueue(&ready_queue, proc);
 }
 
 // scheduler_block_current blocks the currently running process.
@@ -221,10 +241,10 @@ void scheduler_run_next(UserContext *uctxt)
     // Save current process user context
     if (current_process != NULL) {
         current_process->user_context = *uctxt;
-
-        if (current_process->state == PROC_RUNNING) {
-            current_process->state = PROC_READY;
-        }
+        if (current_process != process_get_idle() &&
+            current_process->state == PROC_RUNNING) {
+            scheduler_add(current_process);
+        }   
     }
     // Pick next process
     next = scheduler_next();
