e30b657adba33fd53501bfb8891e672c3687e2a1
Ryan Gonzalez <f006bkc@babylon1.thayer.dartmouth.edu>
2026-05-26 21:00:42 -0400

    read me and init cleanup

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Yalnix Project
 ## Ryan Gonzalez and Sam Hirsh
 
-## Currently at checkpoint 1
+## Currently at checkpoint 4
 
-Just Psuedocode
+Kernel functions work as expected.
+One minor bug in fork, which is that the child resumes at parents delay, which then causes segfault. 
+Test 2 and Test 3 are buggy because of the Fork bug. Currently working on fixing this
+Also getting a warning about stale TLB so need to find where we are misssing a flush
 
 ### Connecting 
 
@@ -30,3 +33,5 @@ echo $YALNIX_FRAMEWORK
 # Building
 
 Make and make clean for rn very simple
+Init.c has tests that will tests, fork, wait, exit, and exec calling itself or another program. 
+Please comment and uncomment sections as testing.
--- src/init.c
+++ src/init.c
@@ -2,103 +2,158 @@
 
 int main(void)
 {
-    //init test
+    /*
+     * TEST 1: Basic init + Delay
+     * Expected:
+     *   init repeatedly prints its pid, then sleeps with Delay.
+     */
     // while (1) {
     //     TracePrintf(0, "INIT RUNNING pid=%d\n", GetPid());
     //     Delay(5);
     // }
-
     // return 0;
-    //  int pid = Fork();
 
-    //fork test 
-    // if (pid == 0) {
-    //     TracePrintf(0, "child: pid=%d\n", GetPid());
-    //     Exit(42);
-    // } else {
-    //     TracePrintf(0, "parent: pid=%d child=%d\n", GetPid(), pid);
-    // }
 
-    // while (1) {
-    //     Delay(2);
-    // }
+    /*
+     * TEST 2: Basic Fork
+     * Expected:
+     *   parent prints its pid and child pid.
+     *   child should print its own pid.
+     */
+
+    int pid;
+    
+    pid = Fork();
+    
+    if (pid == 0) {
+        TracePrintf(0, "child: pid=%d\n", GetPid());
+    } else {
+        TracePrintf(0, "parent: pid=%d child=%d\n", GetPid(), pid);
+    }
+    
+    while (1) {
+        Delay(2);
+    }
+
+
+    /*
+     * TEST 3: Fork + Exit
+     * Expected:
+     *   child exits with status 7.
+     *   "child after Exit SHOULD NOT PRINT" should never print.
+     */
     // int pid;
-
+    //
     // pid = Fork();
-
+    //
     // if (pid == 0) {
     //     TracePrintf(1, "child before Exit\n");
     //     Exit(7);
     //     TracePrintf(1, "child after Exit SHOULD NOT PRINT\n");
     // }
-
+    //
     // TracePrintf(1, "parent after Fork child=%d\n", pid);
-
+    //
     // while (1) {
     //     Delay(2);
     // }
-    //wait and exit 
+
+
+    /*
+     * TEST 4: Fork + Wait + Exit
+     * Expected:
+     *   parent waits for child.
+     *   Wait returns child pid and child's exit status.
+     */
     // int pid;
     // int status;
     // int waited;
-
+    //
     // pid = Fork();
-
+    //
     // if (pid == 0) {
     //     TracePrintf(1, "child: before Exit\n");
     //     Exit(7);
     //     TracePrintf(1, "child: after Exit SHOULD NOT PRINT\n");
     // }
-
+    //
     // TracePrintf(1, "parent: forked child=%d\n", pid);
-
+    //
     // waited = Wait(&status);
-
+    //
     // TracePrintf(1, "parent: Wait returned pid=%d status=%d\n",
     //             waited, status);
-
+    //
     // while (1) {
     //     Delay(2);
     // }
-    //exec test
+
+
+    /*
+     * TEST 5: Exec into init
+     * Expected:
+     *   init reloads itself.
+     *   "init after Exec SHOULD ONLY PRINT IF EXEC FAILED" should not print
+     *   if Exec succeeds.
+     */
     // char *args[] = {"init", NULL};
-
+    //
     // TracePrintf(1, "init before Exec\n");
-
+    //
     // Exec("init", args);
-
+    //
     // TracePrintf(1, "init after Exec SHOULD ONLY PRINT IF EXEC FAILED\n");
-
+    //
     // while (1) {
     //     Delay(2);
     // }
-    // exec and hello world test
+
+
+    /*
+     * TEST 6: Exec into helloworld
+     * Expected:
+     *   init execs into helloworld.
+     *   helloworld should print its startup message.
+     *   "init after Exec SHOULD NOT PRINT" should not print if Exec succeeds.
+     */
     // char *args[] = {"helloworld", NULL};
-
+    //
     // TracePrintf(1, "init before Exec hello\n");
-
+    //
     // Exec("helloworld", args);
-
+    //
     // TracePrintf(1, "init after Exec SHOULD NOT PRINT\n");
+    //
+    // while (1) {
+    //     Delay(2);
+    // }
+
+
+    /*
+     * TEST 7: Long-running parent and child after Fork
+     * Expected:
+     *   parent prints its pid and child pid.
+     *   child should print its own pid and continue delaying.
+     *
+     * Current known issue:
+     *   child may fault after being scheduled due to remaining Fork
+     *   child-continuation bug.
+     */
+    // int pid;
+
+    // pid = Fork();
+
+    // if (pid == 0) {
+    //     TracePrintf(1, "child running pid=%d\n", GetPid());
+
+    //     while (1) {
+    //         Delay(2);
+    //     }
+    // }
+
+    // TracePrintf(1, "parent running pid=%d child=%d\n", GetPid(), pid);
 
     // while (1) {
     //     Delay(2);
     // }
-    int pid;
-
-    pid = Fork();
-
-    if (pid == 0) {
-        TracePrintf(1, "child running pid=%d\n", GetPid());
-
-        while (1) {
-            Delay(2);
-        }
-    }
-
-    TracePrintf(1, "parent running pid=%d child=%d\n", GetPid(), pid);
-
-    while (1) {
-        Delay(2);
-    }
 }
